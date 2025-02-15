be25d894efd7278a1a3d590a4523668b8194ab6e
Kavindi Fernando <it21388248@my.sliit.lk>
2025-02-15 15:54:13 +0530

    Kavindi | Market Volatility

--- betlecare/lib/pages/market/market_volatility.dart
+++ betlecare/lib/pages/market/market_volatility.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:fl_chart/fl_chart.dart';
 
 class MarketVolatilityScreen extends StatelessWidget {
   const MarketVolatilityScreen({super.key});
@@ -23,12 +24,59 @@ class MarketVolatilityScreen extends StatelessWidget {
                     ),
                   ),
                   const SizedBox(height: 20),
+                  SizedBox(
+                    height: 300,
+                    child: PriceLineChart(),
+                  ),
                 ],
               ),
             ),
           ),
         ),
       ],
+    );
+  }
+}
+
+class PriceLineChart extends StatelessWidget {
+  final List<double> prices = [10, 50, 20, 30, 25, 40, 90];
+  final List<String> days = ["Mon", "Tues", "Wed", "Thur", "Fri", "Sat", "Sun"];
+
+  PriceLineChart({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return LineChart(
+      LineChartData(
+        gridData: FlGridData(show: true),
+        titlesData: FlTitlesData(
+          leftTitles: const AxisTitles(
+            sideTitles: SideTitles(showTitles: true, reservedSize: 40),
+          ),
+          bottomTitles: AxisTitles(
+            sideTitles: SideTitles(
+              showTitles: true,
+              getTitlesWidget: (value, meta) {
+                return Text(days[value.toInt()]);
+              },
+              interval: 1,
+            ),
+          ),
+        ),
+        borderData: FlBorderData(show: true),
+        lineBarsData: [
+          LineChartBarData(
+            spots: List.generate(
+              prices.length,
+              (index) => FlSpot(index.toDouble(), prices[index]),
+            ),
+            isCurved: true,
+            color: Colors.blue,
+            dotData: FlDotData(show: true),
+            barWidth: 2,
+          ),
+        ],
+      ),
     );
   }
 }
