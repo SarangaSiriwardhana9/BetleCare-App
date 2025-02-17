dc807d781818a13e6795c94c56a3a110c854e585
Eshan Imesh <111144332+ImeshR@users.noreply.github.com>
2025-02-18 01:34:35 +0530

    update

--- betlecare/lib/supabase_client.dart
+++ betlecare/lib/supabase_client.dart
@@ -1,3 +1,4 @@
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 class SupabaseClientManager {
@@ -9,8 +10,8 @@ class SupabaseClientManager {
   static Future<SupabaseClientManager> get instance async {
     if (_instance == null) {
       await Supabase.initialize(
-        url: 'supabaseUrl',
-        anonKey: 'supabaseAnonKey',
+        url: dotenv.env['SUPABASE_URL']!,
+        anonKey: dotenv.env['SUPABASE_ANON_KEY']!,
       );
       _instance = SupabaseClientManager._();
       _instance!._supabaseClient = Supabase.instance.client;
